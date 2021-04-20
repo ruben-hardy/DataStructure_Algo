f1d03e717f6ec905a5520ea6d3d085aea42d48ce
rubenhardy <rubenhardy.ca@gmail.com>
2021-04-20 17:04:49 -0500

    Modified bracket balancer to support count

--- bracket_balancer.py
+++ bracket_balancer.py
@@ -8,28 +8,42 @@ def bracket_balancer(s: str) -> bool:
         ')':'('
     }
     balanced = True
+    count = 0
     for char in s:
         if char in bracket_dict.values():
             bracket_stream.extend(char)
         else:
-            temp = bracket_stream.pop()
+            if bracket_stream:
+                temp = bracket_stream.pop()
+            else:
+                balanced = False
+                count += 1
+                continue
             if char == ']':
                 if bracket_dict[char] != temp:
                     balanced = False
-                    break
+                    bracket_stream.extend(temp)
+                    count += 1
+                    #break
             elif char == '}':
                 if bracket_dict[char] != temp:
                     balanced = False
-                    break
+                    bracket_stream.extend(temp)
+                    count += 1
+                    #break
             elif char == ')':
                 if bracket_dict[char] != temp:
                     balanced = False
-                    break
-    if balanced:
+                    bracket_stream.extend(temp)
+                    count += 1                    
+                    #break
+    if balanced and len(bracket_stream) == 0:
         print("Balanced")
+        print(count)
     else:
         print('Not balanced')
+        print(count + len(bracket_stream))
 
-a = "[{[()])}]]"
+a = "((]))"
 bracket_balancer(a)
 
